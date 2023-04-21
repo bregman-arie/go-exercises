dd609150f011826f02e9f8d65ed2704a2bf80ba7
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-21 11:31:43 +0330

    add: ctype in main.go

--- exercises/generics_101/main.go
+++ exercises/generics_101/main.go
@@ -1,5 +1,15 @@
 package main
 
+// CType can be int, int32, uint, or uint32
+type CType interface {
+	int | int32 | uint | uint32
+}
+
+// sum1 is the normal function which returns the summation of two CType
+func sum1[T CType](A T, B T) T {
+	return A + B
+}
+
 func main() {
 
 }
